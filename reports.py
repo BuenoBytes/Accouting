278893ecf7f42d2f7f0c35b2484af42d10d62cc3
Ian Bueno <ianbueno.work@gmail.com>
2025-10-30 13:13:40 -0300

    Refactor trial_balance function for clarity and efficiency

--- reports.py
+++ reports.py
@@ -13,30 +13,30 @@ def trial_balance(entities: dict[str, root.Entity], accounts: dict[str, root.Acc
         entity = root.inputopt("Type the desired entity' ID: ", list(entities.keys()))
         select = entities[entity]
         doc += [f"{select.names[0]:<71}{select.id[1] + ' - ' + select.id[2]:>70}"]
-        assets = data.consolidate_ccen(entity, '1')[3] + data.consolidate_ccen(entity, '2')[3] + data.consolidate_ccen(entity, '3')[3]
-        results = data.consolidate_ccen(entity, '4')[3] + data.consolidate_ccen(entity, '5')[3] + data.consolidate_ccen(entity, '6')[3]
-        oprev = -data.consolidate_ccen(entity, '4.1')[3]
-        opexp = -data.consolidate_ccen(entity, '5.1')[3]
-        depr = -data.consolidate_ccen(entity, '5.1.7')[3]
-        re = -data.consolidate_ccen(entity, '4')[3]
-        ex = -data.consolidate_ccen(entity, '5')[3]
-        ass = data.consolidate_ccen(entity, '1')[3]
-        debit = -data.consolidate_ccen(entity, '2.1')[3] / data.consolidate_ccen(entity, '1.1')[3]
-        cash = data.consolidate_ccen(entity, '1.1.1')
     else:
         entity = None
         for k,v in entities.items():
             doc += [f"{v.names[0]:<71}{v.id[1] + ' - ' + v.id[2]:>70}"]
-        assets = data.consolidate_all('1')[3] + data.consolidate_all('2')[3] + data.consolidate_all('3')[3]
-        results = data.consolidate_all('4')[3] + data.consolidate_all('5')[3] + data.consolidate_all('6')[3]
-        oprev = -data.consolidate_all('4.1')[3]
-        opexp = -data.consolidate_all('5.1')[3]
-        depr = -data.consolidate_all('5.1.7')[3]
-        re = -data.consolidate_all('4')[3]
-        ex = -data.consolidate_all('5')[3]
-        ass = data.consolidate_all('1')[3]
-        debit = -data.consolidate_all('2.1')[3] / data.consolidate_all('1.1')[3]
-        cash = data.consolidate_all('1.1.1')
+    def aux(test: str|None, acc_key: str) -> float:
+        return data.consolidate_all(acc_key)[3] if test is None else data.consolidate_ccen(entity, acc_key)[3]
+    assets = aux(entity, '1') + aux(entity, '2') + aux(entity, '3')
+    results = aux(entity, '4') + aux(entity, '5') + aux(entity, '6')
+    oprev = -aux(entity, '4.1')
+    oprev = oprev if oprev!=0 else 0.001
+    opexp = -aux(entity, '5.1')
+    depr = -aux(entity, '5.1.7')
+    re = -aux(entity, '4')
+    re = re if re!=0 else 0.001
+    ex = -aux(entity, '5')
+    ex = ex if ex!=0 else -0.001
+    ass = aux(entity, '1')
+    ass = ass if ass!=0 else 0.001
+    x = -aux(entity, '2.1')
+    y = aux(entity, '1.1')
+    y = y if y!=0 else 0.001
+    cash = data.consolidate_ccen(entity, '1.1.1') if entity else data.consolidate_all('1.1.1')
+    cash[0] = cash [0] if cash[0]!=0 else 0.001
+    debit = x/y
     inconsistency = assets + results
     text = 'C O N S O L I D A T E D   T R I A L   B A L A N C E' if entity is None and len(list(entities.keys())) > 1 else 'T R I A L   B A L A N C E'
     doc += ['_'*ls, '', text.center(ls), '_'*ls, '', f"{'ACCOUNT ID':<19}{'ACCOUNT NAME':<38}{'OPEN BALANCE':>20}{'DEBITS':>18}{'CREDITS':>18}{'CLOSE BALANCE':>20}", '']
@@ -60,16 +60,17 @@ def trial_balance(entities: dict[str, root.Entity], accounts: dict[str, root.Acc
                 warnings += [f"EXPENSES account [{acc_key} - {acc_obj.name}] with a credit closing balance."]
     doc += ['-'*ls, f"Assets difference: {assets:,.2f}", f"Results difference: {results:,.2f}", f"Inconsistency: {inconsistency:,.2f}"]
     fi = re + ex
+    fi = 0.001 if fi== 0.002 else fi
     test = 'Loss' if fi < 0 else 'Profit'
     doc += ['-'*ls,
-            'K E Y   P E R F O R M A N C E   I N D I C A T O R'.center(ls),
-            f"{test:<12}: {fi:,.2f} | {(fi/re)*100:.2f}",
-            f"{'EBIT':<12}: {oprev+opexp:,.2f} | {((oprev+opexp)/oprev)*100:.2f}",
-            f"{'EBITDA':<12}: {oprev+opexp+depr:,.2f} | {((oprev+opexp+depr)/oprev)*100:.2f}",
-            f"{'ROI (%)':<12}: {(fi/(-ex))*100:.2f}",
-            f"{'ROA (%)':<12}: {(fi/ass)*100:.2f}",
-            f"{'Debit ratio':<12}: {debit*100:.2f}",
-            f"{'Cash flow':<12}: {cash[3]-cash[0]:,.2f} | {((cash[3]-cash[0])/cash[0])*100:.2f}"
+            'K E Y   P E R F O R M A N C E   I N D I C A T O R S'.center(ls),
+            f"{test:<12}: {fi:,.2f} | {(fi/re)*100:.2f}%",
+            f"{'EBIT':<12}: {oprev+opexp:,.2f} | {((oprev+opexp)/oprev)*100:.2f}%",
+            f"{'EBITDA':<12}: {oprev+opexp+depr:,.2f} | {((oprev+opexp+depr)/oprev)*100:.2f}%",
+            f"{'ROI (%)':<12}: {(fi/(-ex))*100:.2f}%",
+            f"{'ROA (%)':<12}: {(fi/ass)*100:.2f}%",
+            f"{'Debit ratio':<12}: {debit*100:.2f}%",
+            f"{'Cash flow':<12}: {cash[3]-cash[0]:,.2f} | {((cash[3]-cash[0])/cash[0])*100:.2f}%"
             ]
     if len(warnings) > 2:
         doc += warnings
